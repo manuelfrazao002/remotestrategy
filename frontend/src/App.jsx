f65d381eb3bc44038c6d19a6f0a9ba15e8e9f1aa
manuelfrazao002 <pv23952@alunos.estgv.ipv.pt>
2025-04-13 20:43:56 +0100

    Changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -31,60 +31,56 @@ function App() {
 
         <section id="about-us">
           <h2>Sobre Nós</h2>
-          <img id="img-aboutus" src={aboutUs} alt="sobre_nos_section" />
-          <div id="aboutus-text">
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
-            </p>
+          <div className="aboutus-container">
+            <img id="img-aboutus" src={aboutUs} alt="sobre_nos_section" />
+            <div id="aboutus-text">
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry...
+              </p>
+            </div>
           </div>
         </section>
 
         <section id="cards-section">
-          <h2>Portfólio</h2>
-          <div className="cards-grid">
-            <div className="card">
-              <img src={portfolioImg} alt="Serviço" className="card-img" />
-              <div className="card-content">
-                <button className="more-info-btn">Mais Informações</button>
-              </div>
-            </div>
+  <h2>Portfólio</h2>
+  <div className="cards-grid">
+    <a href="/projeto1" className="card">
+      <img src={portfolioImg} alt="Serviço" className="card-img" />
+      <div className="card-overlay">
+        <p>Mais Detalhes</p>
+      </div>
+    </a>
 
-            <div className="card">
-              <img src={portfolioImg} alt="Serviço" className="card-img" />
-              <div className="card-content">
-                <button className="more-info-btn">Mais Informações</button>
-              </div>
-            </div>
+    <a href="/projeto2" className="card">
+      <img src={portfolioImg} alt="Serviço" className="card-img" />
+      <div className="card-overlay">
+        <p>Mais Detalhes</p>
+      </div>
+    </a>
 
-            <div className="card">
-              <img src={portfolioImg} alt="Serviço" className="card-img" />
-              <div className="card-content">
-                <button className="more-info-btn">Mais Informações</button>
-              </div>
-            </div>
+    <a href="/projeto3" className="card">
+      <img src={portfolioImg} alt="Serviço" className="card-img" />
+      <div className="card-overlay">
+        <p>Mais Detalhes</p>
+      </div>
+    </a>
 
-            <div className="card">
-              <img src={portfolioImg} alt="Serviço" className="card-img" />
-              <div className="card-content">
-                <button className="more-info-btn">Mais Informações</button>
-              </div>
-            </div>
-          </div>
-        </section>
+    <a href="/projeto4" className="card">
+      <img src={portfolioImg} alt="Serviço" className="card-img" />
+      <div className="card-overlay">
+        <p>Mais Detalhes</p>
+      </div>
+    </a>
+  </div>
+</section>
+
 
         <footer id="footer">
           <p>&copy; 2025 Remote Strategy. Todos os direitos reservados.</p>
           <div className="footer-links">
-            <a href="#about-us">Sobre Nós</a> |<a href="#">Contato</a> |
+            <a href="#about-us">Sobre Nós</a>|
+            <a href="#cards-section">Portfolio</a>|
             <a href="/privacidade">Política de Privacidade</a>
           </div>
         </footer>
