788f50eccecf97cd4fdef20e74fc7e35599c02e7
manuelfrazao002 <pv23952@alunos.estgv.ipv.pt>
2025-06-02 22:00:02 +0100

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PoliticaPrivacidade from "./pages/PoliticaPrivacidade";
 import ProjectDetails from "./pages/ProjectDetails";
-import HomePage from "./pages/HomePage"; // <- componente novo com a homepage
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/politica-privacidade" element={<PoliticaPrivacidade />} />
+        <Route path="/politica-privacidade" element={<HomePage />} />
         <Route path="/projeto/:id" element={<ProjectDetails />} />
       </Routes>
     </Router>
