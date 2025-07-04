f24272cb34e93b79226eb2ab13eb1c45fe54bd44
manuelfrazao002 <pv23952@alunos.estgv.ipv.pt>
2025-07-04 10:31:18 +0100

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// 👇 importa esta função
+import path from 'path'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  // 👇 Aqui está o truque importante
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  // 👇 Isto força comportamento SPA
+  server: {
+    historyApiFallback: true,
+  },
 })
